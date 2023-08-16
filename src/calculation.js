20a6f6ba5c5c839aae9c574b410ace065a6108ee
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-16 10:43:17 +0300

    add functions

---
+++ src/calculation.js
@@ -0,0 +1,26 @@
+const getRandomNumber = () => Math.round(Math.random() * 100);
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  return operators[Math.floor(Math.random() * operators.length)];
+}
+
+const getRandomExpression = () => {
+  const firstNumber = getRandomNumber();
+  const operator = getRandomOperator(); 
+  const secondNumber = getRandomNumber();
+  return [firstNumber, operator, secondNumber]
+}
+
+const getResultExpression = (expression) => {
+  const [firstNumber, operator, secondNumber] = expression;
+  switch (operator) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+  }
+}
+
+export {getRandomNumber, getRandomOperator, getResultExpression, getRandomExpression}
