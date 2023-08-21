fdeda5240dc8c43ccc851b3cc133e2de277bb079
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 17:59:49 +0300

    change getQuestion to questionValue

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 
 const roundCount = 3;
 
-const run = (gameRule, getQuestion) => {
+const run = (gameRule, questionValue) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameRule)
   for (let i = 0; i < roundCount; i += 1) {
-    const [question, correctAnswer] = getQuestion
+    const [question, correctAnswer] = questionValue();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
