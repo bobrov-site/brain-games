84b919430213c13b15953187529ca3a527ed0805
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 14:16:01 +0300

    remove getWelcome and getName

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
-import { getWelcome, getName } from "./utils.js";
-
 const runGreeting = () => {
   console.log('Welcome to the Brain Games!');
-  getWelcome();
-  getName();
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default runGreeting
