3c0828fffa5f5c7eff4ba683a1bec5e0bddd62f4
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 13:57:47 +0300

    import progressionGame to progression.js

--- src/index.js
+++ src/index.js
@@ -16,29 +16,4 @@ const runGreeting = () => {
   getName();
 };
 
-const runProgressionGame = () => {
-  getWelcome();
-  const name = getName();
-  getGameRule('What number is missing in the progression?');
-  for (let i = 0; i < roundCount; i += 1) {
-    const progression = getResultProgression();
-    const randomElementFromProgression = takeRandomElementFromProgression(progression);
-    const correctAnswer = randomElementFromProgression;
-    const question = removeElementFromProgression(progression, randomElementFromProgression);
-    getQuestion(question.join(' '));
-    const answer = getAnswer();
-    if (Number(answer) === Number(correctAnswer)) {
-      getSuccess();
-    } else {
-      getFail(answer, correctAnswer, name);
-      break;
-    }
-    if (i === 2) {
-      getWin(name);
-    }
-  }
-};
-
-export {
-  runProgressionGame, runGreeting,
-};
+export { runGreeting };
