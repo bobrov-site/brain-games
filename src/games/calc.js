6194e935a38ac195d19ff3661b6e0646263d1fa3
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 14:01:45 +0300

    refactoring calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,21 +6,21 @@ const runCalcGame = () => {
     const name = getName();
     getGameRule('What is the result of the expression?');
     for (let i = 0; i < roundCount; i += 1) {
-      const expression = getRandomExpression();
-      const question = `${expression[0]} ${expression[1]} ${expression[2]}`;
-      getQuestion(question);
-      const answer = getAnswer();
-      const correctAnswer = getResultExpression(expression);
-      if (Number(answer) === Number(correctAnswer)) {
-        getSuccess();
-      } else {
-        getFail(answer, correctAnswer, name);
-        break;
-      }
-      if (i === 2) {
-        getWin(name);
-      }
+        const expression = getRandomExpression();
+        const question = `${expression[0]} ${expression[1]} ${expression[2]}`;
+        getQuestion(question);
+        const answer = getAnswer();
+        const correctAnswer = getResultExpression(expression);
+        if (Number(answer) === Number(correctAnswer)) {
+            getSuccess();
+        } else {
+            getFail(answer, correctAnswer, name);
+            break;
+        }
+        if (i === 2) {
+            getWin(name);
+        }
     }
-  };
+};
 
 export default runCalcGame
