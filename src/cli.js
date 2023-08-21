7d22f7a3c5fca5db2f370d458dca268a32cd94d4
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 14:32:29 +0300

    import readlineSync in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+import readlineSync from 'readline-sync'; 
 const runGreeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
