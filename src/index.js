2f219cc3f9cc37c5ce860c1eaf1462a8f4bfb6a7
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-21 13:56:22 +0300

    remove prime game and export

--- src/index.js
+++ src/index.js
@@ -39,29 +39,6 @@ const runProgressionGame = () => {
   }
 };
 
-const runPrimeGame = () => {
-  getWelcome();
-  const name = getName();
-  getGameRule('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < roundCount; i += 1) {
-    const question = getRandomNumber();
-    getQuestion(question);
-    const answer = getAnswer();
-    const correctAnswer = isPrimeNumber(question);
-    const correctClientAnswer = correctAnswer === true ? 'yes' : 'no';
-    const checkAnswer = changeYesOrNoFormatAnswerToBoolean(answer);
-    if (checkAnswer === correctAnswer) {
-      getSuccess();
-    } else {
-      getFail(answer, correctClientAnswer, name);
-      break;
-    }
-    if (i === 2) {
-      getWin(name);
-    }
-  }
-};
-
 export {
-  runGcdGame, runProgressionGame, runPrimeGame, runGreeting,
+  runProgressionGame, runGreeting,
 };
