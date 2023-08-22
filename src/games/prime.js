5fa4fd64b369801091d0f7b88626df1223a253ba
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 18:05:51 +0300

    fix runPrimeGame in prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,31 @@
-import { getWelcome, getGameRule, getQuestion, getAnswer, getSuccess, getFail, getWin } from "../cli.js";
-const runPrimeGame = () => {
-    getWelcome();
-    const name = getName();
-    getGameRule('Answer "yes" if given number is prime. Otherwise answer "no".');
-    for (let i = 0; i < roundCount; i += 1) {
-        const question = getRandomNumber();
-        getQuestion(question);
-        const answer = getAnswer();
-        const correctAnswer = isPrimeNumber(question);
-        const correctClientAnswer = correctAnswer === true ? 'yes' : 'no';
-        const checkAnswer = changeYesOrNoFormatAnswerToBoolean(answer);
-        if (checkAnswer === correctAnswer) {
-            getSuccess();
-        } else {
-            getFail(answer, correctClientAnswer, name);
-            break;
+import { getRandomNumber } from '../calculation.js';
+import run from '../index.js';
+
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrimeNumber = (number) => {
+    let isPrime = true;
+    if (number > 1) {
+        for (let i = 2; i < number; i += 1) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
         }
-        if (i === 2) {
-            getWin(name);
-        }
+    } else if (number === 1) {
+        isPrime = false;
     }
+    return isPrime;
 };
+
+const getQuestionValue = () => {
+    const question = getRandomNumber();
+    const correctAnswer = isPrimeNumber(question) === true ? 'yes' : 'no';
+    return [question, String(correctAnswer)]
+}
+
+const runPrimeGame = () => {
+    run(gameRule, getQuestionValue)
+}
 
 export default runPrimeGame
