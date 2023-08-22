4e9a1bae5fe919fb845397e534fc57279e0e02f6
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 18:08:47 +0300

    add getRandomNumber, getRandomIntervalBetween

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
-import readlineSync from 'readline-sync';
+const getRandomNumber = () => Math.round(Math.random() * 100);
+
+const getRandomIntervalBetween = (min, max) => Math.round(Math.random() * (max - min) + min);
+
+export {
+    getRandomNumber, getRandomIntervalBetween,
+};
 
 /* eslint-disable no-console */
