670c8ddaa726afef129454b33941d7cc57ed7081
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 16:53:47 +0300

    fixed runGcdGame

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,25 +1,45 @@
-import { getWelcome, getGameRule, getQuestion, getAnswer, getSuccess, getFail, getWin } from "../cli.js";
-const runGcdGame = () => {
-    getWelcome();
-    const name = getName();
-    getGameRule('Find the greatest common divisor of given numbers.');
-    for (let i = 0; i < roundCount; i += 1) {
-        const question = getRandomNumbersSameParity();
-        const firstNumber = question[0];
-        const secondNumber = question[1];
-        getQuestion(`${firstNumber} ${secondNumber}`);
-        const answer = getAnswer();
-        const correctAnswer = getResultGcd(firstNumber, secondNumber);
-        if (Number(answer) === Number(correctAnswer)) {
-            getSuccess();
-        } else {
-            getFail(answer, correctAnswer, name);
-            break;
-        }
-        if (i === 2) {
-            getWin(name);
-        }
+import run from "../index.js";
+
+import { getRandomNumber } from "../calculation.js";
+
+const gameRule = 'Find the greatest common divisor of given numbers.'
+
+const getEvenRandomNumber = () => getRandomNumber() * 2;
+
+const getOddRandomNumber = () => getEvenRandomNumber() + 1;
+
+const getRandomNumbersSameParity = () => {
+    const numbers = [];
+    let firstNumber = 0;
+    let secondNumber = 0;
+    if (Math.round(Math.random()) === 1) {
+        firstNumber = getEvenRandomNumber();
+        secondNumber = getEvenRandomNumber();
+    } else {
+        firstNumber = getOddRandomNumber();
+        secondNumber = getOddRandomNumber();
     }
+    numbers.push(firstNumber, secondNumber);
+    return numbers;
 };
+
+const getResultGcd = (a, b) => {
+    if (b) {
+        return getResultGcd(b, a % b);
+    }
+    return Math.abs(a);
+};
+
+const getQuestionValue = () => {
+    const question = getRandomNumbersSameParity();
+    const firstNumber = question[0];
+    const secondNumber = question[1];
+    const correctAnswer = getResultGcd(firstNumber, secondNumber)
+    return [`${firstNumber} ${secondNumber}`, String(correctAnswer)]
+}
+
+const runGcdGame = () => {
+    run(gameRule, getQuestionValue)
+}
 
 export default runGcdGame
