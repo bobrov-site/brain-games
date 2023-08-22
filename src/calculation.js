95ae428e7e90e8fadb2741fac19b2fac0ce3fb28
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 17:45:33 +0300

    remove getResultProgression, takeRandomElementFromProgression, removeElementFromProgression

--- src/calculation.js
+++ src/calculation.js
@@ -1,40 +1,6 @@
 const getRandomNumber = () => Math.round(Math.random() * 100);
 
 const getRandomIntervalBetween = (min, max) => Math.round(Math.random() * (max - min) + min);
-
-const getResultProgression = () => {
-  const result = [];
-  const arrayLength = getRandomIntervalBetween(5, 10);
-  const firstNumber = getRandomIntervalBetween(1, 20);
-  const randomProgression = getRandomIntervalBetween(1, 10);
-  result.push(firstNumber);
-  for (let i = 1; i < arrayLength; i += 1) {
-    result.push(0);
-  }
-  for (let i = 1; i < arrayLength; i += 1) {
-    result[i] = result[i - 1] + randomProgression;
-  }
-  return result;
-};
-
-const takeRandomElementFromProgression = (progression) => {
-  const index = getRandomIntervalBetween(0, progression.length - 1);
-  return progression[index];
-};
-
-const removeElementFromProgression = (progression, element) => {
-  const clientProgression = [];
-  const index = progression.findIndex((el) => el === element);
-  for (let i = 0; i < progression.length; i += 1) {
-    if (i !== index) {
-      clientProgression.push(progression[i]);
-    } else {
-      clientProgression.push('..');
-    }
-  }
-  return clientProgression;
-};
-
 const isPrimeNumber = (number) => {
   let isPrime = true;
   if (number > 1) {
@@ -51,6 +17,5 @@ const isPrimeNumber = (number) => {
 };
 
 export {
-  getRandomNumber, getResultProgression, takeRandomElementFromProgression,
-  removeElementFromProgression, isPrimeNumber,
+  getRandomNumber, getRandomIntervalBetween, isPrimeNumber,
 };
