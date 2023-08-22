8375dddb6685ed021789ab91b5703b337859b5f5
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 18:08:21 +0300

    delete calcultation.js

--- src/calculation.js
+++
@@ -1,7 +0,0 @@
-const getRandomNumber = () => Math.round(Math.random() * 100);
-
-const getRandomIntervalBetween = (min, max) => Math.round(Math.random() * (max - min) + min);
-
-export {
-  getRandomNumber, getRandomIntervalBetween,
-};
