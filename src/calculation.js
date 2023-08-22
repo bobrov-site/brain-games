c4fd151c38b20f8932512290de4221d9b8e6b532
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-22 17:52:35 +0300

    remove isPrimeNumber

--- src/calculation.js
+++ src/calculation.js
@@ -1,21 +1,7 @@
 const getRandomNumber = () => Math.round(Math.random() * 100);
 
 const getRandomIntervalBetween = (min, max) => Math.round(Math.random() * (max - min) + min);
-const isPrimeNumber = (number) => {
-  let isPrime = true;
-  if (number > 1) {
-    for (let i = 2; i < number; i += 1) {
-      if (number % i === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-  } else if (number === 1) {
-    isPrime = false;
-  }
-  return isPrime;
-};
 
 export {
-  getRandomNumber, getRandomIntervalBetween, isPrimeNumber,
+  getRandomNumber, getRandomIntervalBetween,
 };
