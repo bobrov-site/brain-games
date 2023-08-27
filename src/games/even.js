ab2672f9028f691085a6773cf4ad955f5c907bde
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-27 15:05:38 +0300

    add interval for getRandomNumber in even.js

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ import getRandomNumber from '../utils.js';
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionValue = () => {
-  const randomNumber = getRandomNumber();
+  const randomNumber = getRandomNumber(0, 100);
   const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
   return [randomNumber, correctAnswer];
 };
