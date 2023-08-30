15ec8936ac4b1b805d2f1ae27a448c7b08f761c1
Daniil Bobrov <allodrmen@yandex.ru>
2023-08-30 13:23:06 +0300

    fix from randomNumber to number

--- src/games/even.js
+++ src/games/even.js
@@ -4,9 +4,9 @@ import getRandomNumber from '../utils.js';
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getRound = () => {
-  const randomNumber = getRandomNumber(0, 100);
-  const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-  return [randomNumber, correctAnswer];
+  const number = getRandomNumber(0, 100);
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  return [number, correctAnswer];
 };
 
 const runEvenGame = () => {
