56a51d2d85c111fd85a39d928295f08205998d42
Daniil Bobrov <allodrmen@yandex.ru>
2023-09-04 16:37:33 +0300

    replaced isEven from getRound in even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,9 +3,10 @@ import getRandomNumber from '../utils.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (a) => a % 2 === 0;
+
 const getRound = () => {
   const question = String(getRandomNumber(0, 100));
-  const isEven = (a) => a % 2 === 0;
   const correctAnswer = isEven(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
