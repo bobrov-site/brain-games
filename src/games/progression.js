360e2748906be996df031abb526884e13902a472
Daniil Bobrov <allodrmen@yandex.ru>
2023-09-04 15:42:24 +0300

    rename from getProgression to buildProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../utils.js';
 
 const gameRule = 'What number is missing in the progression?';
 
-const getProgression = () => {
+const buildProgression = () => {
   const progression = [];
   const length = getRandomNumber(10, 10);
   const startNumber = getRandomNumber(1, 20);
@@ -34,7 +34,7 @@ const removeElementFromProgression = (progression, element) => {
 };
 
 const getRound = () => {
-  const progression = getProgression();
+  const progression = buildProgression();
   const randomElementFromProgression = takeRandomElementFromProgression(progression);
   const correctAnswer = randomElementFromProgression;
   const question = removeElementFromProgression(progression, randomElementFromProgression);
